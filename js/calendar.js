1da384df125f8fb373e59972fb9456ce68c609e2
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2022-05-21 03:08:10 +0200

    changed displayed appts to radio buttons

--- js/calendar.js
+++ js/calendar.js
@@ -189,7 +189,7 @@ function show_events(events, month, day) {
     else {
         // Go through and add each event as a card to the events container
         for(var i=0; i<events.length; i++) {
-            var event_card = $("<div class='event-card'></div>");
+            var event_card = $("<div class='event-card'><input type='radio' name='appt_time' value='"+i+"'></div>");
             var event_name = $("<div class='event-name'>"+events[i]["occasion"]+"</div>");
             var event_count = $("<div class='event-count'>"+events[i]["invited_count"]+" Invited</div>");
             $(event_card).css({ "max-width": "25% "});
